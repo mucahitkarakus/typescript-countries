6b4331076d2cd20db5d9872b6b5232428cd9e611
mucahitkarakus <mocococo.80@gmail.com>
2022-12-07 10:23:42 +0300

    create state and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import axios from "axios"
+import {useState} from "react"
+import {CountryType} from "./type"
+
+
 const App = () => {
+    const [countries, setCountries] = useState<CountryType[]>([])
+    const getCountries = async () => {
+        try {
+            const {data} = await axios.get<CountryType[]>("https://restcountries.com/v2/all");
+            setCountries(data)
+        } catch{
+        console.log("Ülkeleri alırken hata oluştu.");
+        }
+    }
   return (
     <div>Countries App with TypeScript</div>
   )
